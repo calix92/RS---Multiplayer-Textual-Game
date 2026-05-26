3a2681483539a04749ccb97207a92d872cd56a0b
Rodrigo Pinho da Fonseca <rodrigo.pinho.fonseca@ua.pt>
2026-05-26 00:18:38 +0000

    Sistema reconhecimento peers a funcionar com grpc

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-RUN python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. discovery.proto
+RUN python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. multiplayer.proto
 
 ENTRYPOINT ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,89 +1,125 @@
+import argparse
 import asyncio
 import socket
 import uuid
 import grpc
-import discovery_pb2
-import discovery_pb2_grpc
+import multiplayer_pb2
+import multiplayer_pb2_grpc
 
 
 class Peer:
-    def __init__(self, username, host="127.0.0.1", port=None, bootstrap=None):
-        self.username = username
-        self.uuid = str(uuid.uuid4())
-        self.host = host
-        self.port = port or self._free_port()
-        self.bootstrap = bootstrap
-        self.peers = {}
+        def __init__(self, username, host=None, port=None, bootstrap=None):
+                self.username = username
+                self.uuid = str(uuid.uuid4())
+                self.host = host or "127.0.0.1"
+                self.port = port or self._free_port()
+                self.bootstrap = bootstrap
+                self.peers = {}
 
-    def address(self):
-        return f"{self.host}:{self.port}"
+        def address(self):
+                return f"{self.host}:{self.port}"
 
-    def _free_port(self):
-        s = socket.socket()
-        s.bind(("", 0))
-        free_port = s.getsockname()[1]
-        s.close()
-        return free_port
+        def _free_port(self):
+                sock = socket.socket()
+                sock.bind(("", 0))
+                free_port = sock.getsockname()[1]
+                sock.close()
+                return free_port
 
 
-class Discovery(discovery_pb2_grpc.DiscoveryServiceServicer):
-    def __init__(self, peer):
-        self.peer = peer
+class PeerDiscovery(multiplayer_pb2_grpc.PeerDiscoveryServicer):
+        def __init__(self, peer):
+                self.peer = peer
 
-    async def Register(self, request, context):
-        self.peer.peers[request.uuid] = request.address
+        async def RegisterPeer(self, request, context):
+            self.peer.peers[request.uuid] = request.address
+            print(self.peer.peers)
+            return multiplayer_pb2.Empty()
 
-        return discovery_pb2.RegisterResponse(all_peers=self.peers)
-
-    async def GetAllPeers(self, request, context):
-        return discovery_pb2.GetAllPeersResponse(peers=self.peers)
-
-    async def NotifyNewPeer(self, request, context):
-        self.peer.peers[request.uuid] = request.address
-        return discovery_pb2.Empty()
+        async def GetPeers(self, request, context):
+                return multiplayer_pb2.AllPeers(peers=self.peer.peers)
 
 
 class Network:
-    def __init__(self, peer):
-        self.peer = peer
-        self.server = grpc.aio.server()
-        self.service = Discovery(peer)
+        def __init__(self, peer):
+                self.peer = peer
 
-        discovery_pb2_grpc.add_DiscoveryServiceServicer_to_server(
-            self.service,
-            self.server
-        )
 
-        self.server.add_insecure_port(f"[::]:{peer.port}")
+        async def start(self):
+                self.service = PeerDiscovery(self.peer)
+                self.server = grpc.aio.server()
+                multiplayer_pb2_grpc.add_PeerDiscoveryServicer_to_server(
+                        self.service,
+                        self.server
+                )
+                self.server.add_insecure_port(f"0.0.0.0:{self.peer.port}")
 
-    async def start(self):
-        await self.server.start()
+                await self.server.start()
 
-        if self.peer.bootstrap:
-            await self.join()
-        else:
-            self.peer.peers[self.peer.uuid] = self.peer.address()
+                self.peer.peers[self.peer.uuid] = self.peer.address()
+                print(self.peer.peers)
 
-        while True:
-            await asyncio.sleep(1)
+                if self.peer.bootstrap:
+                        await self.join()
 
-    async def join(self):
-        channel = grpc.aio.insecure_channel(self.peer.bootstrap)
-        stub = discovery_pb2_grpc.DiscoveryServiceStub(channel)
+                try:
+                        await self.server.wait_for_termination()
+                except KeyboardInterrupt:
+                        pass
+                finally:
+                        await self.server.stop(grace=1)
 
-        resp = await stub.Register(
-            discovery_pb2.RegisterRequest(
-                uuid=self.peer.uuid,
-                address=self.peer.address(),
-                username=self.peer.username
-            )
-        )
+        async def join(self):
+                channel = grpc.aio.insecure_channel(self.peer.bootstrap)
+                stub = multiplayer_pb2_grpc.PeerDiscoveryStub(channel)
 
-        self.peer.peers.update(resp.all_peers)
-        await channel.close()
+                getpeers = await stub.GetPeers(multiplayer_pb2.Empty())
+                self.peer.peers.update(getpeers.peers)
+                print(self.peer.peers)
+
+                await channel.close()
+
+                for peer_address in self.peer.peers.values():
+                        if peer_address == self.peer.address():
+                                continue
+
+                        channel = grpc.aio.insecure_channel(peer_address)
+                        stub = multiplayer_pb2_grpc.PeerDiscoveryStub(channel)
+
+                        await stub.RegisterPeer(
+                                multiplayer_pb2.Peer(
+                                        uuid=self.peer.uuid,
+                                        address=self.peer.address()
+                                )
+                        )
+
+                        await channel.close()
 
 
 def main():
-    peer = Peer("A")
-    net = Network(peer)
-    asyncio.run(net.start())
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument("--username", required=True)
+        parser.add_argument("--host")
+        parser.add_argument("--port", type=int)
+        parser.add_argument("--bootstrap")
+
+        args = parser.parse_args()
+
+        peer = Peer(
+                username=args.username,
+                host=args.host,
+                port=args.port,
+                bootstrap=args.bootstrap
+        )
+
+        network = Network(peer)
+
+        try:
+                asyncio.run(network.start())
+        except KeyboardInterrupt:
+                print("\nServer stopped by user")
+
+
+if __name__ == "__main__":
+        main()
